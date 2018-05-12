9643013fb9d733fa37caaa97b95a93191ef79b91
lifelessmc <vissmartijn@hotmail.nl>
2018-05-12 17:47:54 +0200

    Added activities and changed their most outside layout to Linear

---
+++ app/src/main/java/koelkastapp/projectgroepje4/com/koelkastapp/KastActivity.java
@@ -0,0 +1,13 @@
+package koelkastapp.projectgroepje4.com.koelkastapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kast);
+    }
+}

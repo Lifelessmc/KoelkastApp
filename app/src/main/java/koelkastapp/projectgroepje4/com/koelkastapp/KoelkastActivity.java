f5eebd0e0294b1e617f60fe0d306bdbc2341b719
lifelessmc <vissmartijn@hotmail.nl>
2018-05-15 09:35:00 +0200

    Small changes

--- app/src/main/java/koelkastapp/projectgroepje4/com/koelkastapp/KoelkastActivity.java
+++ app/src/main/java/koelkastapp/projectgroepje4/com/koelkastapp/KoelkastActivity.java
@@ -2,6 +2,9 @@ package koelkastapp.projectgroepje4.com.koelkastapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class KoelkastActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class KoelkastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_koelkast);
+
     }
 }
